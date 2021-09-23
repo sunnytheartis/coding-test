e9bbe95fe5c2fb661634cac2878c632eeaa4530c
Sun <solskinartwork@gmail.com>
2021-09-22 20:59:40 -0400

    Added HTML, CSS, and JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+var testButton = document.getElementById("#testButton");
+var questions = 
+
+//testButton.addEventListener("click", buttonClick)
+testButton.addEventListener('click', buttonClick)
+function buttonClick(){
+
+    console.log("Button has been clicked")
+
+}
