361e818caf47f20339003ded75d9ec4137435b4a
Sun <solskinartwork@gmail.com>
2021-09-22 21:37:37 -0400

    Adding ways for the questions and answers to show

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 var testButton = document.getElementById("#testButton");
-var questions = 
+var questionEl = [
+    {quest:"What are the questions",
+    firstChoice:"1:"+"Do",
+    secChoice:"2:"+"not",
+    thirdChoice:"3:"+"know",
+    fourthChoice:"4:"+"that",
+    correctAnswer:"4"
+    },
+    {quest:"What are  questions",
+    firstChoice:"1:"+"Do",
+    secChoice:"2:"+"not",
+    thirdChoice:"3:"+"know",
+    fourthChoice:"4:"+"that",
+    correctAnswer:"3"
+    },
+    {quest:"What  the questions",
+    firstChoice:"1:"+"Do",
+    secChoice:"2:"+"not",
+    thirdChoice:"3:"+"know",
+    fourthChoice:"4:"+"that",
+    correctAnswer:"2"
+    },
+    {quest:"What  questions",
+    firstChoice:"1:"+"Do",
+    secChoice:"2:"+"not",
+    thirdChoice:"3:"+"know",
+    fourthChoice:"4:"+"that",
+    correctAnswer:"1"
+    },
+];
 
+function askQuestions(){
+    
+}
 //testButton.addEventListener("click", buttonClick)
 testButton.addEventListener('click', buttonClick)
 function buttonClick(){
 
     console.log("Button has been clicked")
+
 
 }
